26cd77f961ba250177eda9f29b81974b1fe6cc16
SOF <animeshk55@gmail.com>
2018-11-05 03:06:13 +0530

    Aded styles to the navigation button

--- react-native-modal/index.js
+++ react-native-modal/index.js
@@ -40,3 +40,55 @@ const MainAppStack = createStackNavigator({
     },
   },
 });
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.25)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalBody: {
+    backgroundColor: '#fff',
+    width: '80%',
+    padding: 20,
+    borderRadius: 5,
+  },
+});
+
+// NEW CODE BELOW HERE...
+export default class App extends React.Component {
+  state = {
+    modalVisible: false,
+  };
+
+  changeModalVisibility = (modalVisible = false) => {
+    this.setState({ modalVisible });
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <MainAppStack
+          screenProps={{ changeModalVisibility: this.changeModalVisibility }}
+        />
+        <Modal
+          visible={this.state.modalVisible}
+          animationType="fade"
+        >
+          <SafeAreaView style={styles.modalContainer}>
+            <View style={styles.modalBody}>
+              <Text>
+                This is my modal, there are many like it but this one is mine.
+              </Text>
+              <Button
+                title="Close modal"
+                onPress={() => this.changeModalVisibility(false)}
+              />
+            </View>
+          </SafeAreaView>
+        </Modal>
+      </View>
+    );
+  }
+}

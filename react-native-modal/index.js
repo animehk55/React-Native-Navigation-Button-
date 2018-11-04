b582da0dffb445e6d5df4e05508bf56b0fd7ece8
SOF <animeshk55@gmail.com>
2018-11-05 03:02:59 +0530

    Added first navigation component with buton as listed to display on it

--- react-native-modal/index.js
+++ react-native-modal/index.js
@@ -1,3 +1,17 @@
 import React from 'react';
 import { Button, View, SafeAreaView, Modal, Text, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
+
+const Home = ({ navigation, screenProps }) => (
+  <SafeAreaView>
+    <Button
+      title="Go to details"
+      onPress={() => navigation.navigate('Details')}
+    />
+    <Button
+      title="Go to modal"
+      // onPress={() => null}
+      onPress={() => screenProps.changeModalVisibility(true)}
+    />
+  </SafeAreaView>
+);
